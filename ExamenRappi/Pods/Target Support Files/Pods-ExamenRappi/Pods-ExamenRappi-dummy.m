9aad8cef097af4a220d80c3127cf3ad0a8652b19
JuanArcos <j.arcos@alphasoluciones.com>
2019-10-19 14:10:49 -0500

    Add podFile
    Add podfile, and pod configuration

---
+++ ExamenRappi/Pods/Target Support Files/Pods-ExamenRappi/Pods-ExamenRappi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExamenRappi : NSObject
+@end
+@implementation PodsDummy_Pods_ExamenRappi
+@end
---
+++ ExamenRappi/Pods/Target Support Files/Pods-ExamenRappiTests/Pods-ExamenRappiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExamenRappiTests : NSObject
+@end
+@implementation PodsDummy_Pods_ExamenRappiTests
+@end
